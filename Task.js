65cab43a5bcc122cb94c112e924ad94e69b46677
NagendranMurugan <122153142+NagendranMurugan@users.noreply.github.com>
2023-06-20 19:00:15 +0530

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,58 @@
+
+    // function ask(question, yes, no) {
+    //     if (confirm(question)) yes();
+    //     else no();
+    //   }
+      
+    //   ask(
+    //     "Do you agree?",
+    //     function() { alert("You agreed."); },
+    //     function() { alert("You canceled the execution."); }
+    //   );
+      
+    
+
+    // const Task = () =>{
+
+    //     const handleClick = () =>{
+    //         console.log('hello, naga');
+    //     }
+
+    //     const handleClickOnace=(name) =>{
+    //         console.log('hello' + name);
+    //     }
+
+    //     return(
+    //         <div>
+              
+    //           <div className="w-40 h-16 border bg-red-200">
+    //            <button onClick={handleClick}> click </button>
+    //            <br>
+    //            </br><button onclick={()=> {
+    //             handleClickOnace('naga')
+    //            }}>click me Onace</button>
+    //         </div>
+    //         </div>
+    //     )
+    //     };
+         
+
+        const Task = () =>{
+
+           return(
+            <div>
+                <div className="w-40 h-16 border bg-red-200">
+                  submit
+                </div>
+            </div>
+           )
+        }
+        export default Task;
+
+
+
+
+
+
+
+
